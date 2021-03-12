d057dc9fd12a4441f4b7253c8e11b2629cbc80f3
Catouse <catouse@gmail.com>
2021-03-12 23:34:19 +0800

    Fix lint errors.

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,4 @@ module.exports = {
     // see https://jestjs.io/docs/en/configuration.html#testenvironment-string
     // and see https://github.com/axios/axios/issues/1754
     testEnvironment: 'node',
-}
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as utils from './utils';
-export {default as Zentao} from './zentao';
-export {default as Zentao12} from './zentao12';
-export {utils};
+import Zentao from './zentao';
+import Zentao12 from './zentao12';
+
+export {utils, Zentao, Zentao12};
