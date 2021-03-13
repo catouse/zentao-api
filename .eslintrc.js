d5aa4d1a3ad4078c5c4e370c9086155584e83210
Catouse <catouse@gmail.com>
2021-03-13 13:25:13 +0800

    Update eslintrc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-    "extends": [
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended"
-    ],
+  "extends": [
+    "react-app",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  "settings": {
+    "react": {
+      "version": "999.999.999"
+    }
+  }
 }
